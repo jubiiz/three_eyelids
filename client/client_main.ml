30e5473e6c87c9ac14c0918eb09e7fa33f624558
jubiiz <jubiiz.audet@gmail.com>
2024-11-04 21:09:42 -0500

    renamed main to client; minor update.

--- client/client_main.ml
+++ client/client_main.ml
@@ -1,3 +1,5 @@
+exception NotImplemented;;
+
 type sequence_number = {
   increment: unit -> unit;
   get: unit -> int;
@@ -15,13 +17,18 @@ let create_sequence_number () =
 
 let the_sequence_number = create_sequence_number ();;
 
-let rec main () = 
+let rdt_send (msg: string): unit = 
+  raise NotImplemented
+  (*let a_request_packet = form_packet input in*)
+	(* TODO IMPLEMENT *)
+;;
+
+let rec client () = 
   let input = read_line () in
-  the_sequence_number.increment();
-  Printf.printf "The sequence number is: %d and you entered: %s\n" (the_sequence_number.get()) (input); 
-  main ()
+  let _ = rdt_send input in
+  client ()
 ;;
 
 
 print_endline "Hello, World, this is client running!";;
-let () = main();;
+let () = client();;
