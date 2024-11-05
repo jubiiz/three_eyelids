6ba4e9d943832a58c30c87c90f2dad05b49b4771
jubiiz <jubiiz.audet@gmail.com>
2024-11-04 21:06:29 -0500

    setup main loop with sequence number

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # three_eyelids
 Ocaml implementation of a stop-and-wait Reliable Data Transfer (RDT) protocol.
+
+
+## Build
+```
+dune build
+```
+
+## Run the client
+```
+dune exec client
+```
+
+## Run the server
+```
+dune exec server
+```
--- client/client_main.ml
+++ client/client_main.ml
@@ -1,5 +1,27 @@
-let main () = 
-  print_endline "Hello, World, this is client!"
+type sequence_number = {
+  increment: unit -> unit;
+  get: unit -> int;
+  (*equals: int -> bool;*)
+};;
+
+let create_sequence_number () =
+  let counter = ref 0 in
+  {
+  increment = (fun () -> counter := (1 - !counter));
+  get = (fun () -> !counter);
+  (*equals = (fun x -> x = !counter);*)
+  }
 ;;
 
+let the_sequence_number = create_sequence_number ();;
+
+let rec main () = 
+  let input = read_line () in
+  the_sequence_number.increment();
+  Printf.printf "The sequence number is: %d and you entered: %s\n" (the_sequence_number.get()) (input); 
+  main ()
+;;
+
+
+print_endline "Hello, World, this is client running!";;
 let () = main();;
