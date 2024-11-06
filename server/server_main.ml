b0218b74024a9d0ff0ac885fcacfcd4373928570
jubiiz <jubiiz.audet@gmail.com>
2024-11-05 21:34:48 -0500

    Added server main loop

--- server/server_main.ml
+++ server/server_main.ml
@@ -1,5 +1,22 @@
-let main () = 
-  print_endline "Hello World, this is server!"
+open Shared.Sequence_number;;
+
+let the_sequence_number = create_sequence_number ();;
+
+let rdt_recv (msg: string): unit = 
+    let seq_num = the_sequence_number.get () in
+    let _ = print_endline ("Receiving message: " ^ msg ^ " with sequence number: " ^ string_of_int seq_num) in
+    the_sequence_number.increment ()
+    
+  (*let a_request_packet = form_packet input in*)
+	(* TODO IMPLEMENT *)
 ;;
 
-let () = main ();;
+let rec server () = 
+  let input = read_line () in
+  let _ = rdt_recv input in
+  server ()
+;;
+
+
+print_endline "Hello, World, this is server running!";;
+let () = server();;
