4c5c29499a7d2a1036d8b2a1374e9d1093a7ca57
jubiiz <jubiiz.audet@gmail.com>
2024-11-06 17:29:14 -0500

    Added comments for Seb.

--- client/client_main.ml
+++ client/client_main.ml
@@ -1,11 +1,34 @@
 let the_sequence_number = Shared.Sequence_number.create_sequence_number ();;
 
+(*
+type checksum = int;;
+
+type packet = {
+  seq_num: int;
+  data: string;
+  checksum: int;
+}
+
+let get_checksum packet = 
+  magic
+
+let form_packet input = 
+  let seq_num = the_sequence_number.get () in
+  (seq_num, input)
+
+
+let binary_of_packet = 
+  something somethine
+
+*)
+
 let rdt_send (msg: string): unit = 
     let seq_num = the_sequence_number.get () in
+    (* let a_request_packet = form_packet input in*)
+    (*  *)
     let _ = print_endline ("Sending message: " ^ msg ^ " with sequence number: " ^ string_of_int seq_num) in
     the_sequence_number.increment ()
     
-  (*let a_request_packet = form_packet input in*)
 	(* TODO IMPLEMENT *)
 ;;
 
