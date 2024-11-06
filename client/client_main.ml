ffc4a5aa22b41e73706a148e227651153bbb7b52
jubiiz <jubiiz.audet@gmail.com>
2024-11-05 21:30:23 -0500

    Added unit tests for sequence number.

--- client/client_main.ml
+++ client/client_main.ml
@@ -1,7 +1,7 @@
 let the_sequence_number = Shared.Sequence_number.create_sequence_number ();;
+
 let rdt_send (msg: string): unit = 
     let seq_num = the_sequence_number.get () in
-    let _ = the_sequence_number.Shared.Sequence_number.increment () in
     let _ = print_endline ("Sending message: " ^ msg ^ " with sequence number: " ^ string_of_int seq_num) in
     the_sequence_number.increment ()
     
