727eae698a0766d442ac83ab8e8279d02d6ffb52
jubiiz <jubiiz.audet@gmail.com>
2024-11-09 11:11:15 -0500

    fixed server recv

--- server/server_main.ml
+++ server/server_main.ml
@@ -2,19 +2,18 @@ open Shared;;
 
 let the_sequence_number = Sequence_number.create_sequence_number ();;
 
-let rdt_recv (msg: string): unit = 
+let rdt_recv: unit = 
     let seq_num = the_sequence_number.get () in
-    let _ = print_endline ("Receiving message: " ^ msg ^ " with sequence number: " ^ string_of_int seq_num) in
     let response = Udt.recv Udt.server_socket in
-    the_sequence_number.increment (); print_endline ("Received message: " ^ (Bytes.to_string response.data))
+    the_sequence_number.increment (); print_endline ("Received message: " ^ (Bytes.to_string response.data));
+    ignore seq_num
     
   (*let a_request_packet = form_packet input in*)
 	(* TODO IMPLEMENT *)
 ;;
 
 let rec server () = 
-  let input = read_line () in
-  let _ = rdt_recv input in
+  let _ = rdt_recv in
   server ()
 ;;
 
