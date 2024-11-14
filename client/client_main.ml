e0955e81981634e138ed270eb40f0155738930a1
jubiiz <jubiiz.audet@gmail.com>
2024-11-14 13:53:25 -0500

    Implement acks; untested.

--- client/client_main.ml
+++ client/client_main.ml
@@ -1,22 +1,40 @@
 open Shared;;
 
-let the_sequence_number = Sequence_number.create_sequence_number ();;
 let client_socket = Lwt_main.run @@ Udt.get_client_socket ();;
 let server_sockaddr = Unix.ADDR_INET (Udt.server_address, Udt.server_port);;
 
 let explode (msg: string) = List.init (String.length msg) (fun i -> String.make 1 (String.get msg i));;
 
 let rdt_send (msg: string): unit =
-    let rec rdt_send_segment (msg_parts: string list) = 
+    let rec rdt_send_segment (msg_parts: string list) (seq_num: int) = 
         match msg_parts with
         | [] -> () 
-        | next_segment::parts -> let seq_num = the_sequence_number.get () in            
-            let packet = Packet.create seq_num next_segment in
+        | next_segment::_ -> let packet = Packet.create seq_num next_segment in
             let packet_bytes = Packet.bytes_of_packet packet in
             let _ = Udt.send packet_bytes client_socket server_sockaddr in
-            the_sequence_number.increment (); rdt_send_segment parts in
+            rdt_receive_ack msg_parts seq_num
+    and rdt_receive_ack (msg_parts : string list) (seq_num: int) = 
+      let response = Udt.recv client_socket in
+      let packet_bytes = response.data in
+      let packet = Packet.packet_of_bytes packet_bytes in
+      let checksum = Packet.calculate_checksum_from_packet packet in
+      if checksum != packet.checksum then
+          let _ = print_endline "bad checksum" in
+          rdt_send_segment msg_parts seq_num
+      else if packet.data = Bytes.of_string "ACK" then
+        if packet.seq_num = seq_num then
+          let _ = print_endline "bad seq num: re-sending same seq num" in
+          rdt_send_segment msg_parts packet.seq_num 
+        else 
+          let _ = print_endline "ACKed, sending new part" in
+          match msg_parts with 
+          | [] -> ()
+          | _::parts -> rdt_send_segment parts (Sequence_number.increment seq_num)
+      else
+          let _ = print_endline "bad response" in
+          rdt_send_segment msg_parts seq_num in
     let msg_parts = explode msg in
-    rdt_send_segment (msg_parts @ [String.make 1 (Char.chr 0)]) (* Null terminated list *)
+    rdt_send_segment (msg_parts @ [String.make 1 (Char.chr 0)]) 0 (* Null terminated list *)
 ;;
 
 let rec client () = 
--- lib/sequence_number.ml
+++ lib/sequence_number.ml
@@ -4,10 +4,14 @@ type sequence_number = {
   (*equals: int -> bool;*)
 };;
 
+let increment (seq_num: int) =
+  1 - seq_num
+;;
+
 let create_sequence_number () =
   let counter = ref 0 in
   {
-  increment = (fun () -> counter := (1 - !counter));
+  increment = (fun () -> counter := (increment !counter));
   get = (fun () -> !counter);
   (*equals = (fun x -> x = !counter);*)
   }
