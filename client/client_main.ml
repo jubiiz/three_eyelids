e640b1e345d3cdc5aabd9ca4793a0c64fe299731
jubiiz <jubiiz.audet@gmail.com>
2024-11-14 12:34:39 -0500

    Send and receive segmented messages

--- client/client_main.ml
+++ client/client_main.ml
@@ -4,17 +4,24 @@ let the_sequence_number = Sequence_number.create_sequence_number ();;
 let client_socket = Lwt_main.run @@ Udt.get_client_socket ();;
 let server_sockaddr = Unix.ADDR_INET (Udt.server_address, Udt.server_port);;
 
+let explode (msg: string) = List.init (String.length msg) (fun i -> String.make 1 (String.get msg i));;
+
 let rdt_send (msg: string): unit =
-    let seq_num = the_sequence_number.get () in
-    let packet = Packet.create seq_num msg in
-    let packet_bytes = Packet.bytes_of_packet packet in
-    print_endline @@ "Sending message: " ^ msg ^ " with sequence number: " ^ (string_of_int seq_num);
-    let _ = Udt.send packet_bytes client_socket server_sockaddr in
-    the_sequence_number.increment ()
+    let rec rdt_send_segment (msg_parts: string list) = 
+        match msg_parts with
+        | [] -> () 
+        | next_segment::parts -> let seq_num = the_sequence_number.get () in            
+            let packet = Packet.create seq_num next_segment in
+            let packet_bytes = Packet.bytes_of_packet packet in
+            let _ = Udt.send packet_bytes client_socket server_sockaddr in
+            the_sequence_number.increment (); rdt_send_segment parts in
+    let msg_parts = explode msg in
+    rdt_send_segment (msg_parts @ [String.make 1 (Char.chr 0)]) (* Null terminated list *)
 ;;
 
 let rec client () = 
   let input = read_line () in
+    print_endline @@ "Sending message: " ^ input;
   let _ = rdt_send input in
   client ()
 ;;
--- server/server_main.ml
+++ server/server_main.ml
@@ -1,8 +1,10 @@
 open Shared;;
 
 let server_socket = Lwt_main.run @@ Udt.get_server_socket ()
+let null_byte = Bytes.make 1 (Char.chr 0);;
 
-let rdt_recv (): unit = 
+let rdt_recv (): string= 
+  let rec rdt_recv_part (parts: string list) = 
     let response = Udt.recv server_socket in
     let packet_bytes = response.data in
     print_endline ("Received packet with bytes: " ^ (Bytes.to_string packet_bytes));
@@ -15,11 +17,15 @@ let rdt_recv (): unit =
       print_endline ("SEQ NUM: " ^ (string_of_int packet.seq_num));
       print_endline ("DATA LENGTH: " ^ (string_of_int packet.data_length));
       print_endline ("CHECKSUM: " ^ (string_of_int packet.checksum));
-      print_endline ("DATA: " ^ (Bytes.to_string packet.data) ^ "\n")
+      print_endline ("DATA: " ^ (Bytes.to_string packet.data) ^ "\n");
+      if packet.data = null_byte then String.concat "" (List.rev parts)
+      else rdt_recv_part ((Bytes.to_string packet.data)::parts) in
+  rdt_recv_part []
 ;;
 
 let rec server () = 
-  let _ = rdt_recv () in
+  let response = rdt_recv () in
+  print_endline ("Received message: " ^ response);
   server ()
 ;;
 
